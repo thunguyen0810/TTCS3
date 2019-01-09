0108559a6f6d711aca5aaf91f97f2b329c758371
ThuNguyen <thunguyen1918@gmail.com>
2019-01-09 10:01:24 +0700

    Update bai1.cpp

--- bai1.cpp
+++ bai1.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <stdlib.h>
-#include <ctime>
-#include <algorithm>
 #include <string.h>
+#include <ctime>
+#include <windows.h>
 using namespace std;
+
+void textcolor(int x)
+{
+	HANDLE mau;
+	mau=GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(mau,x);
+}
 
 void SapXep(int a[8])
 {
@@ -13,142 +20,99 @@ void SapXep(int a[8])
 				swap(a[i], a[j]);
 }
 
-void Jackpot1()
+void Vietllot(int a[6])
 {
-	cout << "Giai Jackpot 1 : " << "\n";
-	srand((int)time(0));
-	int minN = 1;
-	int maxN = 55;
-	int arr[55];
-	int N = maxN - minN + 1;
-	for(int i = minN; i <= maxN; ++i)
-        arr[i - minN] = i;
-    random_shuffle(arr, arr + N);
-    int take = 6;
-    SapXep(arr);
-    for(int i = 0; i < take; ++i)
-        cout << arr[i] << " ";
-}
-
-void Jackpot2()
-{
-	int a[6], sdb;
-	cout << "Giai Jackpot 2 : " << "\n";
 	srand(time(NULL));
-    for(int i = 1; i < 6; i++)
-    {
-    	a[1] = rand() % 56;
-    	a[2] = rand() % 56;
-    	a[3] = rand() % 56;
-    	a[4] = rand() % 56;
-    	a[5] = rand() % 56;
-	}
-	SapXep(a);
-	for(int i = 1; i < 6; i++)
-		cout << a[i] << " ";
-    cout << endl;
-    sdb = rand() % 56;
-    cout << "So dac biet : " << endl << sdb;
-}
-
-void GiaiNhat()
-{
-	int a[6];
-	cout << "Giai Nhat : " << "\n";
-	srand(time(NULL));
-    for(int i = 1; i < 6; i++)
+    for(int i = 0; i < 6; i++)
     {
     	a[i + 1] = rand() % 56;
 	}
 	SapXep(a);
-	for(int i = 1; i < 6; i++)
-		cout << a[i] << " ";
-    cout << endl;
-}
-
-void GiaiNhi()
-{
-	int a[5];
-	cout << "Giai Nhi : " << "\n";
-	srand(time(NULL));
-    for(int i = 1; i < 5; i++)
-    {
-    	a[1] = rand() % 56;
-    	a[2] = rand() % 56;
-    	a[3] = rand() % 56;
-    	a[4] = rand() % 56;
+	for(int i = 0; i < 6; i++)
+	{
+		textcolor(12);
+		cout << a[i] << "\t";
+		textcolor(15);
 	}
-	SapXep(a);
-	for(int i = 1; i < 5; i++)
-		cout << a[i] << " ";
-	cout << endl;
 }
 
-void GiaiBa()
+void KiemTra(int a[7],int b[7],int sdb)
 {
-	int a[4];
-	cout << "Giai Ba : " << "\n";
-	srand(time(NULL));
-    for(int i = 1; i < 4; i++)
-    {
-    	a[1] = rand() % 56;
-    	a[2] = rand() % 56;
-    	a[3] = rand() % 56;
+	int m = 0, kt = 0; 
+	for(int i = 0; i < 6 ; i++)
+	{
+		for(int j = 0; j < 6; j++)
+		{
+			if(a[i] == b[j])
+				m++;		
+		}
 	}
-	SapXep(a);
-	for(int i = 1; i < 4; i++)
-		cout << a[i] << " ";
-	cout << endl;
-}
-
-void KiemTra()
-{
+	if(m == 6)
+		cout << "BAN DA TRUNG JACKPOT 1!" << endl;
+	else
+		if(m == 4) 
+			cout << "BAN DA TRUNG GIAI NHI!" << endl;
+		else
+			if(m == 3) 
+				cout << "BAN DA TRUNG GIAI BA!" << endl;
+			else
+				if(m == 5)
+				{
+					for(int i = 0; i < 6; i++)
+					{
+						if(b[i] == sdb)
+						{
+							cout << "BAN DA TRUNG JACKPOT 2!" << endl;
+							break;
+						}
+						else
+							cout << "BAN DA TRUNG GIAI NHAT!" << endl;
+					}
+				}
+				else
+					cout << "CHUC BAN MAY MAN LAN SAU !!!!!!" << endl;
 }
 
 int main()
 {
-	int a[7];
+	int a[6];
+	int b[6];
+	
+	textcolor(11);
+	cout << "Ket qua Vietllot hom nay : " << "\n";
+	textcolor(15);
+	Vietllot(a);
+	int sdb = 1 + rand() % 55;
+	
+	textcolor(12);
+	cout << " |\t";
+	textcolor(15);
+	textcolor(14);
+	cout << sdb << endl;
+	textcolor(15);
+	
+	textcolor(13);
 	cout << "Nhap vao cac so trong ve cua ban ";
 	cout << endl;
 	for(int i = 1; i < 7; i++)
 	{
-		cout << "So thu " << i << " : ";
-		cin >> a[i];
+		do
+		{
+			cout << "So thu " << i << " : ";
+			cin >> b[i];
+		}
+		while((b[i] >= 55) || (b[i] < 0));
 	}
 	
 	cout << "Day so cua ban la : " << endl;
 	for(int i = 1; i < 7; i++)
 	{
-		cout << a[i] << " ";
+		cout << b[i] << " ";
 	}
+	cout << endl;
+	
+	KiemTra(a, b, sdb);
 	
 	cout << "\n";
     cout << "---------------------------------------------------------------" << endl;
-    
-    Jackpot1();
-    
-    cout << "\n";
-    cout << "---------------------------------------------------------------" << endl;
-    
-    Jackpot2();
-    
-    cout << "\n";
-    cout << "----------------------------------------------------------------" << endl;
-    
-   	GiaiNhat();
-    
-    cout << "\n";
-    cout << "-----------------------------------------------------------------" << endl;
-    
-    GiaiNhi();
-    
-    cout << "\n";
-    cout << "------------------------------------------------------------------" << endl;
-    
-  	GiaiBa();
-  	
-  	cout << "\n";
-    cout << "------------------------------------------------------------------" << endl;
-  	
-  	KiemTra();
 }
